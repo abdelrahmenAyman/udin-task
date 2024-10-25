8f5c09ec87e6ef1f466b204ea60fdbc780c9f878
Abdelrahmen Elbakary <abdelrahmenayman7@gmail.com>
2024-10-25 07:05:28 +0300

    list champions with base stats route

--- app/routes/champion.py
+++ app/routes/champion.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlmodel import Session
 
-from app import actions, models
+from app import actions
 from app.dependencies import get_session
 from app.schemas.champion import ChampionCreate, ChampionRead
 
@@ -11,3 +11,8 @@ router = APIRouter()
 @router.post("/champions/", status_code=201, response_model=ChampionRead)
 async def create_champion(data: ChampionCreate, session: Session = Depends(get_session)):
     return await actions.Champion.create_with_base_stats(data=data, session=session)
+
+
+@router.get("/champions/", response_model=list[ChampionRead])
+async def list_champions(session: Session = Depends(get_session)):
+    return await actions.Champion.list(session=session)
